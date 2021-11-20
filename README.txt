a7281afcd9f260412973aeb8e583f1e7c81311dc
yann.pedron <yann.pedron@amj-groupe.com>
2021-11-20 21:57:07 +0100

    TEST GAC

---
+++ README.txt
@@ -0,0 +1,3 @@
+Projet de test technique pour GAC
+Réalisé sous symfony 5 avec mysql 8.0.21 et php 7.4.9
+Base de donnée : csv_traitement en UTF8 general ci
---
+++ migrations/Version20211120131409.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211120131409 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE csv (id INT AUTO_INCREMENT NOT NULL, compte_facture INT NOT NULL, numero_facture INT NOT NULL, numero_abonne INT NOT NULL, date DATETIME NOT NULL, heure TIME NOT NULL, dure_volume_reel VARCHAR(255) DEFAULT NULL, dure_volume_facture VARCHAR(255) DEFAULT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE csv');
+    }
+}
